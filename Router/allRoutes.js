c2bd001ee253c929d4705bc7f4ed09c425a52e15
katniss <bourdeloux.corinne@orange.fr>
2024-07-07 00:32:51 +0200

    verification champ nom et prenom

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,12 +5,13 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/signin", "connection", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modification", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
     new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
-    
+    new Route("/menus", "Menus", "/pages/menus.html"),
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
