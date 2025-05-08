970bd0709f5cd6962fbb402ecd868bf2dc9d5ebc
Bourdeloux Corinne <bourdeloux.corinne@orange.fr>
2025-05-08 22:37:13 +0200

    finamisation site vitrine

--- pages/reservations/VideGrenier.php
+++ pages/reservations/VideGrenier.php
@@ -3,21 +3,15 @@
             <h1 class="hero-scene-text">Vide Grenier 2025</h1>
     </div>
 </div>
-
 <br>
 <br>
 <br>
-
 <div class="container text-center pb-3">
-    <br>
-    <a href="/manifestations" class="btn btn-primary">Réservation</a>
+    <a href="#" class="btn btn-primary">Réservation</a>
 </div> 
 
-<div class="full-page"></div>
+<div class="full-page">
 
-    <h2 class="text-center">Liste des réservations</h2>
 <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR2te9l6QycpYyrlB4LIm_KPOmBnbYOVAY85FovGNIK7lPGlVvidho34BSzkGYD8WdzLotFS8Qm9bLU/pubhtml?gid=312686297&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
-<br>
-</div> 
 
-   
+</div>    
